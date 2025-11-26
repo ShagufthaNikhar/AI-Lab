8424d8ad2609537479994d9ddb30a6e03e932cf1
Shaguftha Nikhar <shagufthanikhar@gmail.com>
2025-11-26 19:02:54 +0530

    Add program to calculate factorial of a number

---
+++ Factorial_no.py
@@ -0,0 +1,10 @@
+Write a python program to find factorial of the given number?
+num=int(input('enter the number ::')) #num=5
+fact=1
+for i in range(1,num+1):
+ fact=fact*i
+print("The Factorial of 5 is:",fact)
+
+Output:
+enter the number :: 5
+The Factorial of 5 is: 120
